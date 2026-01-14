50ef11439ec21d0e787e70a825c7341562f28731
Kamohelo <kamohelotshabalala@proton.me>
2026-01-14 23:39:28 +0200

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -17,8 +17,22 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from ip_tracking.views import login_view
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="IP Tracking API",
+        default_version='v1',
+        description="Security and Analytics API",
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', login_view),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0)),
 ]
